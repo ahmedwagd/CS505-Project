c8b1c88032d9f17e573c7d4ebdc794edfc7fae69
Ahmed Wagdy <31709375+ahmedwagd@users.noreply.github.com>
2024-01-12 06:23:34 +0200

    Update MarathonRegistration.cpp

--- MarathonRegistration.cpp
+++ MarathonRegistration.cpp
@@ -1,113 +1,182 @@
 #include "MarathonRegistration.h"
 
-MarathonQueue::MarathonQueue(): front(NULL), rear(NULL)
+MarathonQueue::Node::Node(const Runner& r) : runner(r), next(NULL)
+{
+}
+
+MarathonQueue::MarathonQueue() : front(NULL), rear(NULL)
 {
 }
 
 MarathonQueue::~MarathonQueue()
 {
-    while (!isEmpty()) 
+	while (!isEmpty())
 	{
-        dequeue();
-    }
+		dequeue();
+	}
+
 }
 
-void MarathonQueue::enqueue(const Runner& runner) 
+void MarathonQueue::enqueue(const Runner& runner)
 {
-    Node* newNode = new Node(runner);
-    if (isEmpty()) 
+	Node* newNode = new Node(runner);
+	if (isEmpty())
 	{
-        front = rear = newNode;
-    }
+		front = rear = newNode;
+	}
 	else
 	{
-        rear->next = newNode;
-        rear = newNode;
-    }
+		rear->next = newNode;
+		rear = newNode;
+	}
 }
 
-void MarathonQueue::dequeue() 
+void MarathonQueue::dequeue()
 {
-    if (isEmpty())
+	if (isEmpty())
 	{
-        return;
-    }
-
-    Node* temp = front;
-    front = front->next;
-    delete temp;
+		cout << "!!!!!!!!!!!!Mafesh 7aga hena\n";
+		return;
+	}
+	Node* temp = front;
+	front = front->next;
+	delete temp;
 }
 
-bool MarathonQueue::isEmpty() const 
+bool MarathonQueue::isEmpty() const
 {
-    return front == NULL;
+	return front == NULL;
 }
 
-void MarathonQueue::displayQueue() const 
+void MarathonQueue::displayQueue() const
 {
-    Node* current = front;
-    while (current != NULL) 
+	Node* current = front;
+	while (current != NULL)
 	{
-        current->runner.displayRunner();
-        current = current->next;
-    }
-}
-
-bool MarathonQueue::searchRunner(const string& name) const 
-{
-    Node* current = front;
-    while (current != NULL) 
-	{
-        if (current->runner.getName() == name) 
-		{
-            current->runner.displayRunner();
-            return true;
-        }
-        
+		current->runner.displayRunner();
 		current = current->next;
-    }
-    return false;
+	}
 }
 
-void MarathonQueue::editRunner(const string& name, int newAge, const string& newCountry) 
+bool MarathonQueue::searchRunnerByName(const string& name) const
 {
-    Node* current = front;
-    while (current != NULL)
+	Node* current = front;
+	while (current != NULL)
 	{
-        if (current->runner.getName() == name) 
+		if (current->runner.getName() == name)
 		{
-            current->runner.setAge(newAge);
-            current->runner.setCountry(newCountry);
-            break;
-        }
-        current = current->next;
-    }
+			cout << "Gebtholak ->\n";
+			current->runner.displayRunner();
+			return true;
+		}
+		current = current->next;
+	}
+	return false;
 }
 
-void MarathonQueue::deleteRunner(const string& name) 
+void MarathonQueue::editRunnerByName(const string& name, int newId, int newAge, const string& newGender, int newWeight, const string& newCountry)
 {
-    if (isEmpty()) 
+	Node* current = front;
+	while (current != NULL)
 	{
-        return;
-    }
-
-    if (front->runner.getName() == name) 
-	{
-        dequeue();
-        return;
-    }
-
-    Node* current = front;
-    while (current->next != NULL && current->next->runner.getName() != name) 
-	{
-        current = current->next;
-    }
-
-    if (current->next != NULL) 
-	{
-        Node* temp = current->next;
-        current->next = current->next->next;
-        delete temp;
-    }
+		if (current->runner.getName() == name)
+		{
+			current->runner.setId(newId);
+			current->runner.setAge(newAge);
+			current->runner.setGender(newGender);
+			current->runner.setWeight(newWeight);
+			current->runner.setCountry(newCountry);
+			break;
+		}
+		current = current->next;
+	}
 }
 
+void MarathonQueue::deleteRunnerByName(const string& name)
+{
+	if (isEmpty())
+	{
+		cout << "Mesh mawgod 7ad hena b el esm da\n";
+		return;
+	}
+	if (front->runner.getName() == name)
+	{
+		dequeue();
+		cout << "Tmama msa7toh\n";
+		return;
+	}
+	// feha kalam !!!
+	Node* current = front;
+	while (current->next !=NULL && current->next->runner.getName() != name)
+	{
+		current = current->next;
+	}
+	if (current->next != NULL)
+	{
+		Node* temp = current->next;
+		current->next = current->next->next;
+		delete temp;
+	}
+}
+
+bool MarathonQueue::searchRunnerById(int id) const
+{
+	Node* current = front;
+	while (current != NULL)
+	{
+		if (current->runner.getId() == id)
+		{
+			cout << "Gebtholak ->\n";
+			current->runner.displayRunner();
+			return true;
+		}
+
+		current = current->next;
+	}
+	return false;
+}
+
+void MarathonQueue::editRunnerById(int id, const string& newName, int newAge, const string& newGender, int newWeight, const string& newCountry)
+{
+	Node* current = front;
+	while (current != NULL)
+	{
+		if (current->runner.getId() == id)
+		{
+			current->runner.setName(newName);
+			current->runner.setAge(newAge);
+			current->runner.setGender(newGender);
+			current->runner.setWeight(newWeight);
+			current->runner.setCountry(newCountry);
+			break;
+		}
+		current = current->next;
+	}
+}
+
+void MarathonQueue::deleteRunnerById(int id)
+{
+	if (isEmpty())
+	{
+		cout << "Mesh mawgod 7ad hena b el ID da\n";
+		return;
+	}
+	if (front->runner.getId() == id)
+	{
+		dequeue();
+		cout << "Tmama msa7toh\n";
+		return;
+	}
+	// feha kalam !!!
+	Node* current = front;
+	while (current->next != NULL && current->next->runner.getId() != id)
+	{
+		current = current->next;
+	}
+	if (current->next != NULL)
+	{
+		Node* temp = current->next;
+		current->next = current->next->next;
+		delete temp;
+	}
+}
