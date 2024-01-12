5a8c4ea8f4d9bccc3efa494ba3775634c6565eeb
Ahmed Wagdy <31709375+ahmedwagd@users.noreply.github.com>
2024-01-12 06:25:22 +0200

    Update Runner.cpp

--- Runner.cpp
+++ Runner.cpp
@@ -1,11 +1,21 @@
 #include "Runner.h"
 
-Runner::Runner(const string& n, int a, const string& c): name(n), age(a), country(c)
+
+Runner::Runner(int i, const string& n, int a, const string& g, int w, const string& c) :id(i), name(n), age(a), gender(g), weight(w), country(c)
+{
+}
+Runner::~Runner()
 {
 }
 
-Runner::~Runner()
+void Runner::setId(int i)
 {
+	this->id = i;
+}
+
+int Runner::getId()
+{
+	return this->id;
 }
 void Runner::setName(string n)
 {
@@ -26,6 +36,28 @@ int Runner::getAge()
 {
 	return this->age;
 }
+
+void Runner::setGender(string g)
+{
+	this->gender = g;
+}
+
+string Runner::getGender()
+{
+	return this->gender;
+}
+
+void Runner::setWeight(int w)
+{
+	this->weight = w;
+}
+
+
+int Runner::getWeight()
+{
+	return this->weight;
+}
+
 void Runner::setCountry(string c)
 {
 	this->country = c;
@@ -36,7 +68,12 @@ string Runner::getCountry()
 	return this->country;
 }
 
-void Runner::displayRunner() 
+void Runner::displayRunner()
 {
-    cout << "Name: " << this->getName() << "\tAge: " << this->getAge() << "\tCountry: " << this->getCountry() << endl;
+	cout << "Runner no.: " << this->getId()
+		<< "\tName: " << this->getName() 
+		<< "\tAge: " << this->getAge() 
+		<< "\tGender: " << this->getGender()
+		<< "\tWeight: " << this->getWeight()
+		<< "\tCountry: " << this->getCountry() << endl;
 }
