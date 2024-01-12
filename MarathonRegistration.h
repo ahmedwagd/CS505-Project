b5a17668471017d88dbcafb03f15f7d0162f3c5e
Ahmed Wagdy <31709375+ahmedwagd@users.noreply.github.com>
2024-01-12 06:24:37 +0200

    Update MarathonRegistration.h

--- MarathonRegistration.h
+++ MarathonRegistration.h
@@ -9,25 +9,32 @@ using namespace std;
 class MarathonQueue
 {
 private:
-    struct Node {
-        Runner runner;
-        Node* next;
-        Node(const Runner& r) : runner(r), next(NULL) {}
-    };
-    Node* front;
-    Node* rear;
-
+	static int counter;
+	struct Node
+	{
+		Runner runner;
+		Node* next;
+		Node(const Runner& r);
+	};
+	Node* front;
+	Node* rear;
 public:
 	MarathonQueue();
 	~MarathonQueue();
 
-    void enqueue(const Runner& runner);
-    void dequeue();
-    bool isEmpty() const;
-    void displayQueue() const;
-    bool searchRunner(const string& name) const;
-    void editRunner(const string& name, int newAge, const string& newCountry);
-    void deleteRunner(const string& name);
+	void enqueue(const Runner& runner);
+	void dequeue();
+	bool isEmpty() const;
+	void displayQueue() const;
+
+	bool searchRunnerByName(const string& name) const;
+	void editRunnerByName(const string& name, int newId, int newAge, const string& newGender, int newWeight, const string& newCountry);
+	void deleteRunnerByName(const string& name);
+
+	bool searchRunnerById(int id) const;
+	void editRunnerById(int id, const string& newName, int newAge, const string& newGender, int newWeight, const string& newCountry);
+	void deleteRunnerById(int id);
+
 };
 
 #endif
